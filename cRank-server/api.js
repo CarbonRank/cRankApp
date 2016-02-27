7befed5b8883ed71ad234e2325065ee077b274f1
Arif Topcu <09.arif@gmail.com>
2016-02-27 00:14:04 -0500

    more apis for vehicle selection

--- cRank-server/api.js
+++ cRank-server/api.js
@@ -11,12 +11,52 @@ module.exports = function(app, request, parseString) {
                         response.results.push(parseInt(arr[i].value[0]));
                     }
                     res.send(response);
-                } else {
-                    res.send(err);
-                }
+                } else res.send(err);
             });
         });
-        
+    });
+
+    app.get('/api/vehicle/make', function(req, res) {
+        var year = req.query.year;
+        if(year) {
+            var url = 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year=' + year;
+            request(url, function(error, response, body) {
+                parseString(body, function(err, result) {
+                    if(!err) {
+                        var response = {results:[]}
+                        var arr = result.menuItems.menuItem;
+                        for(var i = 0; i<arr.length; i++) {
+                            response.results.push(arr[i].value[0]);
+                        }
+                        res.send(response);
+                    } else res.send(err);
+                });
+            });
+        } else {
+            res.send(false);
+        }
+    });
+
+    app.get('/api/vehicle/model', function(req, res) {
+        var year = req.query.year;
+        var model = req.query.model;
+        if(year && model) {
+            var url = 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=' + year + '&make=' + model;
+            request(url, function(error, response, body) {
+                parseString(body, function(err, result) {
+                    if(!err) {
+                        var response = {results:[]}
+                        var arr = result.menuItems.menuItem;
+                        for(var i = 0; i<arr.length; i++) {
+                            response.results.push(arr[i].value[0]);
+                        }
+                        res.send(response);
+                    } else res.send(err);
+                });
+            });
+        } else {
+            res.send(false);
+        }
     });
 
     app.get('/api/something', function(req, res) {
