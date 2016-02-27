0a4f367396448b6c88b9c585609ae9f96decde9d
Dylan Fischler <dylanfischler@gmail.com>
2016-02-27 04:13:52 -0500

    stuff

--- cRank/www/js/controllers/DriveCtrl.js
+++ cRank/www/js/controllers/DriveCtrl.js
@@ -1,5 +1,12 @@
-app.controller('DriveCtrl', function($scope, $state) {
+app.controller('DriveCtrl', function($scope, $state, $ionicModal) {
 	$scope.drive_state = 0;
+
+	$ionicModal.fromTemplateUrl('templates/post-drive.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
 
 	$scope.$on('startDriving', function(){
 		$scope.drive_state = 1;
@@ -8,5 +15,28 @@ app.controller('DriveCtrl', function($scope, $state) {
 
 	$scope.$on('endDriving', function(){
 		$scope.drive_state = 0;
+		$scope.modal.show();
+
+
+		  // $scope.openModal = function() {
+		  //   $scope.modal.show();
+		  // };
+		  // $scope.closeModal = function() {
+		  //   $scope.modal.hide();
+		  // };
+		  // //Cleanup the modal when we're done with it!
+		  // $scope.$on('$destroy', function() {
+		  //   $scope.modal.remove();
+		  // });
+		  // // Execute action on hide modal
+		  // $scope.$on('modal.hidden', function() {
+		  //   console.log("bye modal")
+		  // });
+		  // // Execute action on remove modal
+		  // $scope.$on('modal.removed', function() {
+		  //   // Execute action
+		  // });
+
+
 	});
 });
--- cRank/www/js/controllers/MapCtrl.js
+++ cRank/www/js/controllers/MapCtrl.js
@@ -1,29 +1,94 @@
-app.controller('MapCtrl', function($scope, $cordovaGeolocation, $ionicLoading) {
+app.controller('MapCtrl', function($scope, $cordovaGeolocation, $ionicLoading, uiGmapGoogleMapApi, $interval) {
 	var options = {timeout: 10000, enableHighAccuracy: true};
-	 
+
+	$scope.location = {};
+
+	var updateLocation = function(cb){
+		$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+			$scope.location = position.coords;
+			if(cb) cb(position.coords);
+		}, function(error){
+	      alert("Could not get location");
+	    });
+	};
+
+	$interval(function() {
+	        updateLocation();
+	}, 1000);
+
+
+	$scope.$on('initMap', function(){
+		$scope.$emit('loadingEvent', { action: 'start' });
+
+		updateLocation(function(position){
+			$scope.map = { 
+	    		center: { latitude: $scope.location.latitude, longitude: $scope.location.longitude }, 
+	    		zoom: 18,
+	    		disableDefaultUI: true,
+	    		control: {}
+	    	};
+	    	$scope.$emit('loadingEvent', { action: 'end' });
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	var updateLocation = function(cb){
+		$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+			$scope.map = { 
+				center: { latitude: $scope.location.latitude, longitude: $scope.location.longitude }, 
+				zoom: 18,
+				disableDefaultUI: true,
+				control: {}
+			};
+			cb();
+		}, function(error){
+	      alert("Could not get location");
+	    });
+	};
 
 	$scope.$on('initMap', function(){
 		console.log("map rec driv");
 		$scope.$emit('loadingEvent', { action: 'start' });
 
+		var updateMap = function(){
+			updateLocation(function(){
+				$scope.$emit('loadingEvent', { action: 'end' });
+			});
+		    // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+		    // 	$scope.map = { 
+		    // 		center: { latitude: position.coords.latitude, longitude: position.coords.longitude }, 
+		    // 		zoom: 18,
+		    // 		disableDefaultUI: true,
+		    // 		control: {}
+		    // 	};
+
+		    // 	$scope.$emit('loadingEvent', { action: 'end' });
+
+		    // 	// uiGmapGoogleMapApi.then(function(maps) {
+		    // 	// 	maps.event.trigger( $scope.map.control.getGMap(), 'resize');
+		    // 	// });
+		    
+		    // }, function(error){
+		    //   alert("Could not get location");
+		    // });
+			
+		};
+
 		ionic.Platform.ready(function(){
-	        console.log("ready??");
-
-	        $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
-	        	$scope.map = { 
-	        		center: { latitude: position.coords.latitude, longitude: position.coords.longitude }, 
-	        		zoom: 18,
-	        		disableDefaultUI: true
-	        	};
-
-	        	$scope.$emit('loadingEvent', { action: 'end' });
-	        
-	        }, function(error){
-	          alert("Could not get location");
-	        });
-	    });
-	});
-
-	
+			updateMap();
+		});
+	});*/
 });
