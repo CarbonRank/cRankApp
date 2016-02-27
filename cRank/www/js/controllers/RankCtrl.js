1ae1ac5a3929f2c08a9b51aa7092284a34d42db2
Dylan Fischler <dylanfischler@gmail.com>
2016-02-27 15:00:31 -0500

    rank stuff

--- cRank/www/js/controllers/RankCtrl.js
+++ cRank/www/js/controllers/RankCtrl.js
@@ -1,5 +1,23 @@
-app.controller('RankCtrl', function($scope) {
+app.controller('RankCtrl', function($scope, $http) {
+	$http({
+	    method: 'GET',
+	    url: '/api/user/sorted',
+	}).success(function (result) {
+	    $scope.rankedUsers = result;
+	});
 
+	var randomNumber = function(){
+		// return Math.floor(Math.random() * 100);
+
+		return Math.floor(Math.random() * 50);
+	};
+
+	$scope.randomImage = function(){
+		var str = "";
+		str += "http://api.randomuser.me/portraits/men/" + randomNumber() + ".jpg";
+
+		return str;
+	};
   
 
 });
