0f6a2568ae67287229111e8942baa3a5e412eb28
d4rkmage80 <d4rkmage80@gmail.com>
2016-02-27 02:17:52 -0500

    starting dropdown

--- cRank/www/js/controllers/RegisterCtrl.js
+++ cRank/www/js/controllers/RegisterCtrl.js
@@ -1,3 +1,13 @@
-app.controller('RegisterCtrl', function($scope) {
-	
+app.controller('RegisterCtrl', function($scope, $http) {
+    $scope.selectedVehicle = null;
+    $scope.vehicleSelection = [];
+
+    $http({
+            method: 'GET',
+            url: '/api/vehicle/year',
+            data: { applicationId: 1 }
+    }).success(function (result) {
+    	console.log("result", result);
+        $scope.vehicleSelection = result;
+    });
 });
