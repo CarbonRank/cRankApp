3055acc1c740d52e7744b26d6775254c7660b84a
d4rkmage80 <d4rkmage80@gmail.com>
2016-02-27 04:36:04 -0500

    added vehicle reg

--- cRank/www/js/controllers/RegisterCtrl.js
+++ cRank/www/js/controllers/RegisterCtrl.js
@@ -1,13 +1,35 @@
 app.controller('RegisterCtrl', function($scope, $http) {
-    $scope.selectedVehicle = null;
-    $scope.vehicleSelection = [];
+    	$scope.vehicleYear = null;
+    	$scope.vehicleMake = null;
+    	$scope.vehicleModel = null;
+    	$scope.vehicleYearSelection = [];
+    	$scope.vehicleMakeSelection = [];
+    	$scope.vehicleModelSelection = [];
 
-    $http({
+    	$http({
             method: 'GET',
             url: '/api/vehicle/year',
-            data: { applicationId: 1 }
-    }).success(function (result) {
-    	console.log("result", result);
-        $scope.vehicleSelection = result;
-    });
-});
+    	}).success(function (result) {
+        	$scope.vehicleYearSelection = result.results;
+    	});
+
+    	$scope.getMake = function () {
+    		$http({
+            	method: 'GET',
+            	url: '/api/vehicle/make?year=' + $scope.vehicleYear,
+    		}).success(function (result) {
+        		$scope.vehicleMakeSelection = result.results;
+    		});
+		}
+
+		$scope.getModel = function () {
+    		$http({
+            	method: 'GET',
+            	url: '/api/vehicle/model?year=' + $scope.vehicleYear + '&make=' + $scope.vehicleMake,
+    		}).success(function (result) {
+        		$scope.vehicleModelSelection = result.results;
+        		console.log("result",result);
+    		});
+		}
+	}
+);
