f04fb1bf639d0850b7fd8a5bd95877c6f2d6a519
Zach Milrod <Zach@eduroam122-220.hampshire.edu>
2016-02-27 00:11:59 -0500

    added connection to database

--- cRank-server/server.js
+++ cRank-server/server.js
@@ -3,8 +3,23 @@ var app            	= express();
 var bodyParser     	= require('body-parser');
 var methodOverride 	= require('method-override');
 var request 		= require('request');
+var fs = require('fs');
 
 var port = process.env.PORT || 3000;
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var dbpassword = fs.readFileSync('db').toString().split('\n')[0];
+var url = 'mongodb://crankadmin:'+dbpassword+'@ds017688.mlab.com:17688/crank';
+
+MongoClient.connect(url, function(err, db) {
+	if (err) {
+	    console.log('Unable to connect to the mongoDB server. Error:', err);
+	} 
+	else{
+	   	console.log('Connection established to', url);
+	    db.close();
+  }
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
@@ -15,6 +30,6 @@ require('./api')(app, request);
 
 app.listen(port);
 
-console.log('cRanking that shit up at' + port);
+console.log('cRanking that shit up at ' + port);
 
 exports = module.exports = app;
