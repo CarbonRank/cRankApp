8060215f7a03050ab55d557dbde407df3d65ad05
Zach Milrod <Zach@eduroam122-220.hampshire.edu>
2016-02-27 05:20:09 -0500

    made new user info send to database and link to the vehicle registration page

--- cRank/www/js/controllers/UserInfoCtrl.js
+++ cRank/www/js/controllers/UserInfoCtrl.js
@@ -6,7 +6,7 @@ app.controller('UserInfoCtrl', function($scope, $http, $state) {
 	$scope.processForm = function() {
 	  $http({
 	  method  : 'POST',
-	  url     : 'api/user/',
+	  url     : '/api/user/newuser',
 	  data    : $scope.formData,  // pass in data as strings
 	  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
 	 })
@@ -22,7 +22,7 @@ app.controller('UserInfoCtrl', function($scope, $http, $state) {
 	    //   $scope.message = data.message;
 	    // }
 
-
+	    $state.go('registration');
 
 	  });
 	};
