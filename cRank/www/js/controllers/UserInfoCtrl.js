07ddbe59f74689ec1d223426aa46c9747acddb65
Dylan Fischler <dylanfischler@gmail.com>
2016-02-27 07:01:53 -0500

    refactored scss

--- cRank/www/js/controllers/UserInfoCtrl.js
+++ cRank/www/js/controllers/UserInfoCtrl.js
@@ -4,27 +4,27 @@ app.controller('UserInfoCtrl', function($scope, $http, $state) {
 
 	// process the form
 	$scope.processForm = function() {
-		debugger;
-	  $http({
-	  method  : 'POST',
-	  url     : '/api/user/newuser',
-	  data    : $scope.formData  // pass in data as strings
-	 })
-	  .success(function(data) {
-	    console.log("data", data);
+		$state.go('registration');
 
-	    // if (!data.success) {
-	    //   // if not successful, bind errors to error variables
-	    //   $scope.errorName = data.errors.name;
-	    //   $scope.errorSuperhero = data.errors.superheroAlias;
-	    // } else {
-	    //   // if successful, bind success message to message
-	    //   $scope.message = data.message;
-	    // }
-	   
-	    $state.go('registration');
+		/*$http({
+			method  : 'POST',
+			url     : '/api/user/newuser',
+			data    : $scope.formData  // pass in data as strings
+		})
+		.success(function(data) {
+			console.log("data", data);
 
-	  });
+			// if (!data.success) {
+			//   // if not successful, bind errors to error variables
+			//   $scope.errorName = data.errors.name;
+			//   $scope.errorSuperhero = data.errors.superheroAlias;
+			// } else {
+			//   // if successful, bind success message to message
+			//   $scope.message = data.message;
+			// }
+
+			$state.go('registration');
+		});*/
 	};
 
 });
